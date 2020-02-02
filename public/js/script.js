68058c719127c127c69ce0e5b1818e0775035151
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-02-03 01:28:20 +0530

    moved index.html in public and abble add titles and cliking on them they are getiing highlighted

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,19 @@
+const highlight = function(item) {
+  document.querySelector('.highlight').classList.remove('highlight');
+  item.classList.add('highlight');
+};
+
+const showTitleItems = function() {
+  const title = event.target;
+  console.log(title);
+  highlight(title);
+};
+
 const addTodoTitle = () => {
   const navigationBar = document.querySelector('#navigationBar');
   const h2 = document.createElement('h2');
   const titleHolder = document.getElementById('titleBox');
+  h2.addEventListener('click', showTitleItems);
   h2.innerText = titleHolder.value;
   titleHolder.value = '';
   h2.classList.add('todoHeading');
