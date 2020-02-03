a75dd2b4145e7bc3acf2f08b0f68b6f7686f90a6
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-02-03 10:42:43 +0530

    removed server's connection as working on front end

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const http = require('http');
 const { requestListener } = require('./lib/handlers');
 
 const main = (port = 3000) => {
-  const server = new http.Server(requestListener);
-  server.listen(port, () => console.log(`listening at :${port}`));
+  // const server = new http.Server(requestListener);
+  // server.listen(port, () => console.log(`listening at :${port}`));
+  console.log('will connect shortly');
 };
 
 main(process.argv[2]);
