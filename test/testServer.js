a574da97e4bae5b72e39a1b534801137726acf0a
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-02-05 14:26:46 +0530

    added test on deleting item

--- test/testServer.js
+++ test/testServer.js
@@ -49,3 +49,15 @@ describe('funcName', function() {
     fs.unlinkSync(`${__dirname}/testTodoList.json`);
   });
 });
+
+describe('funcName', function() {
+  it('should give home page / path', function(done) {
+    request(requestListener)
+      .post('/deleteItem')
+      .send('titleId=t1580877440369&itemId=i1580877444596')
+      .expect(200, done);
+  });
+  after(() => {
+    fs.unlinkSync(`${__dirname}/testTodoList.json`);
+  });
+});
