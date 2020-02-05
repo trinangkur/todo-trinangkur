6ad1ab18107522ce4d200712b6fa03398af23dcf
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-02-05 14:24:03 +0530

    added test

--- test/testServer.js
+++ test/testServer.js
@@ -1,4 +1,10 @@
 const request = require('supertest');
+const fs = require('fs');
+
+fs.writeFileSync(
+  `${__dirname}/testTodoList.json`,
+  '{"t1580877440369":{"name":"hey","id":"t1580877440369","tasks":{"i1580877444596":{"id":"i1580877444596","text":"how are you?","status":false}}}}'
+);
 const { requestListener } = require('../lib/handlers');
 
 describe('Home Page', function() {
@@ -7,7 +13,39 @@ describe('Home Page', function() {
       .get('/')
       .set('Accept', '*/*')
       .expect(200)
-      .expect('Content-Type', 'text/html', done)
-      .expect(/TODO/);
+      .expect('Content-Type', 'text/html')
+      .expect('Content-Length', '1105')
+      .expect(/TODO/, done);
+  });
+});
+
+describe('GET Todo', function() {
+  it('should give home page / path', function(done) {
+    request(requestListener)
+      .get('/getTodoList')
+      .set('Accept', '*/*')
+      .expect(200, done);
+  });
+});
+
+describe('POST addTodoTitle', function() {
+  it('should give home page / path', function(done) {
+    request(requestListener)
+      .post('/addTodoTitle')
+      .send(`title=hallo`)
+      .set('Accept', '*/*')
+      .expect(200, done);
+  });
+});
+
+describe('funcName', function() {
+  it('should give home page / path', function(done) {
+    request(requestListener)
+      .post('/markItem')
+      .send('titleId=t1580877440369&itemId=i1580877444596')
+      .expect(200, done);
+  });
+  after(() => {
+    fs.unlinkSync(`${__dirname}/testTodoList.json`);
   });
 });
