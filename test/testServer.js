3248097cbfba4ed62e6cb5a3775d3c99cf3cd5a6
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-02-05 14:28:19 +0530

    added test on deleting title

--- test/testServer.js
+++ test/testServer.js
@@ -45,15 +45,21 @@ describe('funcName', function() {
       .send('titleId=t1580877440369&itemId=i1580877444596')
       .expect(200, done);
   });
-  after(() => {
-    fs.unlinkSync(`${__dirname}/testTodoList.json`);
-  });
 });
 
 describe('funcName', function() {
   it('should give home page / path', function(done) {
     request(requestListener)
       .post('/deleteItem')
+      .send('titleId=t1580877440369&itemId=i1580877444596')
+      .expect(200, done);
+  });
+});
+
+describe('funcName', function() {
+  it('should give home page / path', function(done) {
+    request(requestListener)
+      .post('/deleteTodoTitle')
       .send('titleId=t1580877440369&itemId=i1580877444596')
       .expect(200, done);
   });
