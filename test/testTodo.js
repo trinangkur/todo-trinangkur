247e03b33bef85fb3ba4002597e9b86dc282a38f
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-02-10 17:25:55 +0530

    added test for static method load of todoCollection

---
+++ test/testTodo.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const {TodoCollection} = require('../lib/todo');
+
+describe('TodoCollection', function() {
+  describe('load', function() {
+    it('should return empty object, when empty string is given', function() {
+      assert.deepStrictEqual(TodoCollection.load(''), new TodoCollection());
+    });
+  });
+});
