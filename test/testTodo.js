f5b76f811f2696541e8bb25aabaee2125583c404
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-02-10 17:48:28 +0530

    [revthi, trinangkur] added test, in method load of todo class...

--- test/testTodo.js
+++ test/testTodo.js
@@ -6,6 +6,24 @@ describe('TodoCollection', function() {
     it('should return empty object, when empty string is given', function() {
       assert.deepStrictEqual(TodoCollection.load(''), new TodoCollection());
     });
-    it('should ')
+    it('should get same object for given same json', function() {
+      const json = {
+        t1581337001157: {
+          name: 'hey',
+          id: 't1581337001157',
+          tasks: {
+            i1581337003892: {
+              id: 'i1581337003892',
+              text: 'hey you',
+              status: false
+            }
+          }
+        }
+      };
+      assert.deepStrictEqual(
+        TodoCollection.load(JSON.stringify(json)),
+        TodoCollection.load(JSON.stringify(json))
+      );
+    });
   });
 });
