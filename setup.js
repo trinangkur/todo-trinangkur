a5d2d959aa3a2202b3fceed3d638046c816bb200
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-02-12 16:50:11 +0530

    [revathi,trinangkur] added hasfeild handler for every post handler

--- setup.js
+++ setup.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const setupFilePath = function() {
-  console.log('hey hye');
   if (!fs.existsSync('data/todoList.json')) {
     fs.mkdirSync('./data');
     fs.writeFileSync('./data/todoList.json', '');
